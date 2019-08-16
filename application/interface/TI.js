8869d94cf953cd3db1f24da0ff0488b94af7ca7d
wyx <1993020111@qq.com>
2019-08-16 14:19:37 +0800

    add wui demo

--- application/interface/TI.js
+++ application/interface/TI.js
@@ -1,11 +1,38 @@
 import React from 'react'
 import {
   Text,
-  View
+  View,
+  ScrollView
 } from 'react-native'
 import { 
   SafeAreaView
 } from 'react-navigation' 
+import {
+  Warticle,
+  Wcard,
+  Wcardone,
+  Wcategory,
+  Wcode,
+  Wcomment,
+  Wdate,
+  Wgradual,
+  Wheader,
+  Whelp,
+  WimageCard,
+  Wloading,
+  Wmessage,
+  Wlogin,
+  Wregister,
+  Wpsbox,
+  Wquantity,
+  Wsearch,
+  Wspinner,
+  Wswitch,
+  Wtab,
+  WtimeSelect,
+  Wtips,
+  Wtitle
+} from 'react-native-wui'
 
 class TI extends React.Component{
   constructor(props){
@@ -18,7 +45,27 @@ class TI extends React.Component{
   render(){
     return(
       <SafeAreaView>
-        <Text>TI</Text>
+        <ScrollView 
+          showsVerticalScrollIndicator={false}>
+          <Warticle/>
+          <Wcard/>
+          <Wcardone/>
+          <Wcategory/>
+          <Wcomment/>
+          <Wdate/>
+          <Wheader/>
+          <Whelp/>
+          <WimageCard/>
+          <Wloading/>
+          <Wquantity/>
+          <Wsearch/>
+          <Wspinner/>
+          <Wswitch/>
+          <Wtab/>
+          <WtimeSelect/>
+          <Wtitle/>
+          <View style={{height:60,width:'100%'}}></View>
+        </ScrollView>
       </SafeAreaView>
     )
   }
